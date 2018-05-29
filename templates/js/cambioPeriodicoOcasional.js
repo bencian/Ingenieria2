4c1de2df27d140def8e92fb9fd116e9e80548725
bencian <bencian94@gmail.com>
2018-05-29 14:42:22 -0300

    empezado el publicar viaje

---
+++ templates/js/cambioPeriodicoOcasional.js
@@ -0,0 +1,12 @@
+function cambio(){
+	texto = document.getElementById("botonCambio").textContent;
+	if(texto == "Cambiar a periodico"){
+		document.getElementById("botonCambio").textContent = "Cambiar a ocasional";
+		document.getElementById("ocasional").style.display = "none";
+		document.getElementById("periodico").style.display = "block";
+	} else {
+		document.getElementById("botonCambio").textContent = "Cambiar a periodico";
+		document.getElementById("ocasional").style.display = "block";
+		document.getElementById("periodico").style.display = "none";		
+	}
+}
